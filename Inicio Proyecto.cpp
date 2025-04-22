2dbf4c08e697ccd8160565c0ce97203afaf33614
Jesús Suárez <165787819+DarkYizus@users.noreply.github.com>
2025-04-22 12:15:47 -0400

    Add files via upload
    Inicio del codigo del proyecto

---
+++ Inicio Proyecto.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <random>
+
+// Definiciones para las cartas de póker
+const int PALO_CORAZONES = 0;
+const int PALO_DIAMANTES = 1;
+const int PALO_TREBOLES = 2;
+const int PALO_PICAS = 3;
+
+const int VALOR_2 = 0;
+const int VALOR_3 = 1;
+const int VALOR_4 = 2;
+const int VALOR_5 = 3;
+const int VALOR_6 = 4;
+const int VALOR_7 = 5;
+const int VALOR_8 = 6;
+const int VALOR_9 = 7;
+const int VALOR_10 = 8;
+const int VALOR_J = 9;
+const int VALOR_Q = 10;
+const int VALOR_K = 11;
+const int VALOR_A = 12;
+
+const char* palos_str[] = {"Corazones", "Diamantes", "Tréboles", "Picas"};
+const char* valores_str[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
